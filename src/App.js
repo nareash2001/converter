f31b0f3e4898c2152b4c285b4bf45d7208ba2894
nareash2001 <nareash2001@gmail.com>
2022-02-26 14:51:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+  function cm()
+  {
+ var feat=document.getElementById('feet').value;
+ var centi=feat*30.48
+ console.log(centi);
+ document.getElementById('output').value=centi;
+  }
+  function m()
+  {
+ var feat=document.getElementById('feet').value;
+ var centi=feat*0.3048
+ console.log(centi);
+ document.getElementById('output').value=centi;
+
+  }
+  function km()
+  {
+ var feat=document.getElementById('feet').value;
+ var centi=feat*0.0003048
+ console.log(centi);
+ document.getElementById('output').value=centi;
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <center>
+    <h1 className='header'>Feet Conversion</h1>
+    <input type="number" placeholder='Enter value' id='feet'/>
+    <button className='Centimeter' >Foot</button><br/>
+    <button onClick={cm}> Centimeter </button>
+    <button onClick={m}>Meter</button>
+    <button onClick={km}>Kilometer</button>
+    <br/>
+    <input id='output'/>
+    </center>
     </div>
+
   );
 }
 
